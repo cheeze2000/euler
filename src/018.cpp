1fe77696d6ec96189ba883eea1b3825caa4a9bb4
Nicholas <me@cheeze2000.net>
2020-12-25 16:01:46 +0800

    problem 018

--- src/018.cpp
+++ src/018.cpp
@@ -6,11 +6,9 @@ int dp[15][15] = {};
 
 int d(int i, int j) {
   if (i < 0 || i > 14 || j < 0 || j > 14) return 0;
-  if (dp[i][j] > 0) return dp[i][j];
-
-  dp[i][j] = max(d(i - 1, j - 1), d(i - 1, j)) + xs[i][j];
   return dp[i][j];
 }
+
 
 int main() {
   ifstream file("inputs/018.txt");
@@ -21,9 +19,16 @@ int main() {
     }
   }
 
+  dp[0][0] = xs[0][0];
+  for (int i = 1; i < 15; i++) {
+    for (int j = 0; j <= i; j++) {
+      dp[i][j] = max(d(i - 1, j - 1), d(i - 1, j)) + xs[i][j];
+    }
+  }
+
   int ans = 0;
   for (int i = 0; i < 15; i++) {
-    ans = max(ans, d(14, i));
+    ans = max(ans, dp[14][i]);
   }
 
   cout << ans << "\n";
